63a13fdf6e09f399397a6dd3fa1707c836219a3d
bit <2656585116@qq.com>
2025-12-01 15:57:48 +0800

    添加项目文件。

---
+++ code2.1/myfirst.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>								// a PREPROCESSOR directive
+int main()										// functin header
+{												// start of function body
+	using namespace std;						//	make definitions visible
+	cout << "Come up and C++ me some time.";	// message
+	cout << endl;								// start a new line
+	cout << "You won't regret it!" << endl;		// more output
+	return 0;									// terminate main()
+}												// end of function body
+
